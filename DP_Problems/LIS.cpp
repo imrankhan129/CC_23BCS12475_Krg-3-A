75a5372e0c32a468e2f73bf650d57314188e9632
imrankhan129 <157881079+imrankhan129@users.noreply.github.com>
2026-04-09 12:48:35 +0530

    Create LIS.cpp

---
+++ DP_Problems/LIS.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i=0;i<nums.size();i++){
+            if(ans.back()<nums[i]){
+                ans.push_back(nums[i]);
+            }
+            else{
+                int idx = lower_bound(ans.begin(), ans.end(),nums[i]) - ans.begin();
+                ans[idx] = nums[i];
+                }
+        }
+        return ans.size();
+    }
+};
