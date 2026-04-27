790689b2a9d0303d391a4f85751a3b002c564537
imrankhan129 <157881079+imrankhan129@users.noreply.github.com>
2026-04-27 14:28:25 +0530

    Question - 03

---
+++ Lab_EST/Aggresive_Cows.cpp
@@ -0,0 +1,37 @@
+// Aggresive cows
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, c;
+        cin >> n >> c;
+        vector<int> stalls(n);
+        for (int i=0;i<n;i++){
+            cin>>stalls[i];
+        }
+        sort(stalls.begin(), stalls.end());
+        int low=1, high=stalls[n - 1] - stalls[0], ans = 0;
+        while(low<=high){
+            int mid=(low + high)/2;
+            int count=1,last_position=stalls[0];
+            for(int i=1;i<n;i++){
+                if(stalls[i] - last_position >= mid){
+                    count++;
+                    last_position = stalls[i];
+                }
+            }
+            if(count>=c){
+                ans=mid;
+                low=mid+1;
+            } 
+            else{
+                high=mid-1;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
